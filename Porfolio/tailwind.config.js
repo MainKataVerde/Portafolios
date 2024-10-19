d78930cfcfe6ee523d8ccab23004fde9aa53217b
MainKataVerde <melancatkit@gmail.com>
2024-10-19 13:59:57 +0200

    Estructura de modo ordenador echa , falta la del movil

--- Porfolio/tailwind.config.js
+++ Porfolio/tailwind.config.js
@@ -4,16 +4,7 @@ module.exports = {
   theme: {
     extend: {
       keyframe: {
-        subir: {
-          "0%": {
-            transform: "translateY(0px)",
-            opacity: "1",
-          },
-          "100%": {
-            transform: "translateY(-100px)",
-            opacity: "0",
-          },
-        },
+        subir: {},
       },
     },
   },
